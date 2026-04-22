86887bf138453c01d5b46458c30ef987b02a9ca4
yujin <jinjini000@naver.com>
2026-04-22 15:27:20 +0900

    첫커밋

--- app/domains/meeting/router.py
+++ app/domains/meeting/router.py
@@ -1,1 +1,11 @@
 # app\domains\meeting\router.py
+# from fastapi import APIRouter, Depends
+# from sqlalchemy.orm import Session
+# from app.infra.database.session import get_db
+# from . import service, schemas
+
+# router = APIRouter(prefix="/meeting", tags=["Meeting"])
+
+# @router.post("/", response_model=schemas.DomainResponse)
+# async def create_endpoint(data: schemas.DomainCreate, db: Session = Depends(get_db)):
+#     return await service.DomainService.handle_api_request(data, db)
